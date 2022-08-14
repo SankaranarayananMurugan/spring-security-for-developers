5eceaca3906e9ce3cadcda194be875530b107ce6
SankaranarayananMurugan <m.sankaranarayanan@outlook.com>
2022-08-15 04:06:37 +0530

    Permit public APIs

--- src/main/java/com/facadecode/spring/security/config/ApiSecurityConfig.java
+++ src/main/java/com/facadecode/spring/security/config/ApiSecurityConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.facadecode.spring.security.constant.SecurityConstants.PUBLIC_API_LIST;
+import static org.springframework.http.HttpMethod.GET;
+
 @Configuration
 public class ApiSecurityConfig {
     @Bean
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(auth -> auth
+                        .antMatchers(GET, PUBLIC_API_LIST).permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic();
